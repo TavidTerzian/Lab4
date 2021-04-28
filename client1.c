87b67d2f662bf8fcd862e582f55edf51cf0626a2
darthtav <davidterzian8@gmail.com>
2021-04-27 19:21:28 -0700

    Connection through IP and Port established

---
+++ client1.c
@@ -0,0 +1,49 @@
+/*  Make the necessary includes and set up the variables.  */
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main()
+{
+    int sockfd;
+    int len;
+    struct sockaddr_in address;
+    int result;
+    char ch = 'A';
+    char* ip = "10.0.2.15";
+
+/*  Create a socket for the client.  */
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+/*  Name the socket, as agreed with the server.  */
+
+    address.sin_family = AF_INET;
+    address.sin_port = htons(5000);
+    inet_pton(AF_INET, ip, &address.sin_addr);
+    len = sizeof(address);
+
+/*  Now connect our socket to the server's socket.  */
+
+    result = connect(sockfd, (struct sockaddr *)&address, len);
+
+    if(result == -1) {
+        perror("oops: client1");
+        exit(1);
+    }
+
+/*  We can now read/write via sockfd.  */
+
+    write(sockfd, &ch, 1);
+    read(sockfd, &ch, 1);
+    printf("char from server = %c\n", ch);
+    close(sockfd);
+    exit(0);
+}
